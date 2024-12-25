26bc1179d96fbe695970db5bbf06688fba7a0bdd
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-12-25 20:50:49 +0700

    connect to database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const config = require('./config/config.js')
 const webRoutes = require('./routes/web.js')
+const mysql = require('mysql2')
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -11,6 +12,22 @@ const HOST_NAME = process.env.HOST_NAME
 config(app)
 
 app.use('/test', webRoutes)
+
+//test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    database: 'baduy',
+    password: '123456'
+})
+
+connection.query(
+    'select * from Users',
+    function (err, results, fields) {
+        console.log(">>>check results", results)
+    }
+)
 
 app.listen(PORT, HOST_NAME, () => {
     console.log(`Example app listening on port ${PORT}`)
